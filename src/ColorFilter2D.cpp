3a24ee560b6365f42316f872a52facb76d8f4fd8
Sahand Yousefpour <sahand.yousefpour@tum.de>
2015-12-04 17:34:52 +0100

    Moved HSV Interactive to GUI
    All the elements of HSV control panel is moved to the *VisionConfig* tab
    inside GUI.
    Changes in the control panel would be sent to the *color_filter_2d* node
    via **ROS Messages**.

--- src/ColorFilter2D.cpp
+++ src/ColorFilter2D.cpp
@@ -5,6 +5,10 @@
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
+#include <std_msgs/Int32.h>
+#include <std_msgs/MultiArrayLayout.h>
+#include <std_msgs/MultiArrayDimension.h>
+#include <std_msgs/Int32MultiArray.h>
 
 #include <pcl_ros/point_cloud.h>
 #include <pcl/filters/passthrough.h>
@@ -14,27 +18,81 @@
 
 #include "HSVFilter.hpp"
 
-
 // Subscriber to the incoming RGB image (object received via ImageTransport
 image_transport::Subscriber image_sub_;
 // HSV-thresholded image Publisher
 image_transport::Publisher hsv_pub_;
-HSVFilter hsv_filter_;
-int h_min_ = 12;
-int h_max_ = 22;
-int s_min_ = 130;
-int s_max_ = 255;
-int v_min_ = 140;
-int v_max_ = 255;
-int er_sz_ = 1, dl_sz_ = 3;
-int er_it_ = 1, dl_it_ = 1;
+// subscriber to receive HSV Interactive mode state
+ros::Subscriber hsvInteractive_sub_;
+// subscriber to receive HSV Values from GUI
+ros::Subscriber hsvValue_sub_;
 
-void on_trackbar(int, void*) {
-  hsv_filter_.setColorValues(h_min_, h_max_, s_min_, s_max_, v_min_, v_max_);
-  hsv_filter_.setMorphKernels(er_sz_, dl_sz_);
-  hsv_filter_.setMorphIters(er_it_, dl_it_);
+bool interactive_ = false;
+
+HSVFilter hsv_filter_;
+
+// callbacks
+void image_cb_(const sensor_msgs::ImageConstPtr& msg);
+void set_interactive_(const std_msgs::Int32::ConstPtr& msg);
+void set_hsv_values_(const std_msgs::Int32MultiArray::ConstPtr& msg);
+
+
+int main (int argc, char **argv) {
+  // Initialize ROS
+  ros::init (argc, argv, "color_filter");
+  // ROS Node Handle
+  ros::NodeHandle nh_;
+  // Instance which is capable of receiving RGB image
+  image_transport::ImageTransport it_(nh_);
+  // Subscrive to input video feed and publish output video feed
+  image_sub_ = it_.subscribe("/camera/rgb/image_raw", 1, image_cb_);
+  hsvInteractive_sub_ = nh_.subscribe("toggle_hsv_mode", 1, set_interactive_);
+  hsvValue_sub_ = nh_.subscribe("hsv_values", 10, set_hsv_values_);
+
+  hsv_pub_ = it_.advertise("crops/vision/image_filter/hsv_filtered", 1);
+
+  ros::spin();
+  return 0;
 }
 
+/**
+ * Callback that receives the message to toggle HSV interactive mode
+*/
+void set_interactive_(const std_msgs::Int32::ConstPtr& msg) {
+  int state = msg->data;
+  interactive_ = (state != 0);
+  std::cout << "interactive mode: " << interactive_ << std::endl;
+  // if(interactive_)
+  //   create_control_panel();
+  // else
+  //   destroy_control_panel();
+}
+
+/**
+ * Callback that receives the message containing new HSV filter values.
+ * CAUTION: order of attributes in the message
+ *        0. Hue_Min
+ *        1. Hue_Max
+ *        2. Saturation_Min
+ *        3. Saturation_Max
+ *        4. Value_Min
+ *        5. Value_Max
+ *        6. Erosion_Size
+ *        7. Erosion_Iterations
+ *        8. Dilation_Size
+ *        9. Dilation_Iterations
+ */
+void set_hsv_values_(const std_msgs::Int32MultiArray::ConstPtr& msg) {
+  hsv_filter_.setHsvValues(
+    msg->data[0], msg->data[1], msg->data[2],
+    msg->data[3], msg->data[4], msg->data[5]);
+  hsv_filter_.setMorphKernels(msg->data[6], msg->data[8]);
+  hsv_filter_.setMorphIters(msg->data[7], msg->data[9]);
+}
+
+/**
+ * Callback that receives the message containing the rgb iamge
+*/
 void image_cb_(const sensor_msgs::ImageConstPtr& msg) {
   cv_bridge::CvImagePtr cv_ptr;
   try {
@@ -46,47 +104,12 @@ void image_cb_(const sensor_msgs::ImageConstPtr& msg) {
   }
   // HSV filtering based on the input color
   hsv_filter_.setImage(cv_ptr->image);
-  //hsv_filter_.setImage(cv_ptr->image);
-  // TODO remove hard-coded color value
-  // hsvFilter.setColorValues("RED");
-  cv::Mat thresholdImage = hsv_filter_.getFilteredImage();
+  cv::Mat thresholdImage = hsv_filter_.getFiltered();
+  // cv::Mat thresholdImage = hsv_filter_.getFilteredImage(crops_vision::YELLOW);
   hsv_filter_.morphOps(thresholdImage);
   // Output modified video stream
   cv_ptr->image = thresholdImage;
   cv_ptr->encoding = sensor_msgs::image_encodings::MONO8; // output is a single-channel image
   hsv_pub_.publish(cv_ptr->toImageMsg());
   cv::waitKey(30);
-}
-
-int main (int argc, char **argv) {
-  // Initialize ROS
-  ros::init (argc, argv, "color_filter");
-
-  // Ros Node Handle
-  ros::NodeHandle nh_;
-  // Instance which is capable of receiving RGB image
-  image_transport::ImageTransport it_(nh_);
-  // Subscrive to input video feed and publish output video feed
-  image_sub_ = it_.subscribe("/camera/rgb/image_raw", 1, image_cb_);
-  hsv_pub_ = it_.advertise("crops/vision/image_filter/hsv_filtered", 1);
-
-  std::cout << "interactive mode: ON" << std::endl;
-  // Setup the trackbars
-  cv::namedWindow("HSV Range Controller", CV_WINDOW_AUTOSIZE);
-  cv::createTrackbar("H_MIN", "HSV Range Controller", &h_min_, 179, on_trackbar);
-  cv::createTrackbar("H_MAX", "HSV Range Controller", &h_max_, 179, on_trackbar);
-  cv::createTrackbar("S_MIN(%)", "HSV Range Controller", &s_min_, 255, on_trackbar);
-  cv::createTrackbar("S_MAX(%)", "HSV Range Controller", &s_max_, 255, on_trackbar);
-  cv::createTrackbar("V_MIN(%)", "HSV Range Controller", &v_min_, 255, on_trackbar);
-  cv::createTrackbar("V_MAX(%)", "HSV Range Controller", &v_max_, 255, on_trackbar);
-  cv::createTrackbar("Erosion Kernel\nSize: 2n+1", "HSV Range Controller", &er_sz_, 21, on_trackbar);
-  cv::createTrackbar("Erosion Iterations", "HSV Range Controller", &er_it_, 5, on_trackbar);
-  cv::createTrackbar("Dilation Kernel\nSize: 2n+1", "HSV Range Controller", &dl_sz_, 21, on_trackbar);
-  cv::createTrackbar("Dilation Iterations", "HSV Range Controller", &dl_it_, 5, on_trackbar);
-  // Initialize trackbars
-  on_trackbar(0, 0);
-  // Spin
-  ros::spin ();
-
-  return 0;
 }
